7a9b61528569361afcd4c08091cd7b5d0258555a
AbhinavYdv <abhinavyadavdev@gmail.com>
2024-03-05 01:57:49 +0530

    Template Ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Homepage } from './Components/homepage'
+import { Calendar } from './Components/Calendar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <div className="container">
+       <div className="columns">
+         <div className="column is-half is-offset-one-quarter">
+           <Routes>
+             <Route exact path="/" element={<Homepage />} />
+
+             <Route path="/calendar" element={<Calendar />} />
+
+           </Routes>
+         </div>
+       </div>
+     </div>
+   </Router>
   );
 }
 
---
+++ src/Components/Calendar.js
@@ -0,0 +1,8 @@
+export const Calendar = () => {
+    return (
+      <div>
+        <h1>Calendar</h1>
+        <p>This is the calendar</p>
+      </div>
+    );
+}
